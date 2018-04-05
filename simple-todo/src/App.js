0547c1a5a34402749a4d028908f9558883e55c1f
rckmnz <rckmnz05@gmail.com>
2018-04-05 16:29:48 -0400

    initiated files for simple-todo

--- simple-todo/src/App.js
+++ simple-todo/src/App.js
@@ -1,54 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const TodoItem = ({text}) => (
-  <li>{text}</li>
-);
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [],
-      newTodo: ''
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const todos = [...this.state.todos, this.state.newTodo];
-    this.setState({todos, newTodo: ''});
-  }
   render() {
-    const {newTodo} = this.state;
-    const todos = this.state.todos.map((t, i) => (
-      <TodoItem key={i} text={t} />
-    ));
     return (
       <div className="App">
-        <h1>Simple Todo App</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            className="todo-input"
-            autoComplete="off"
-            type="text"
-            name="newTodo"
-            placeholder="What needs to be done?"
-            value={newTodo}
-            onChange={(e) => this.setState({[e.target.name]: e.target.value })}
-          />
-          <button
-            type="submit"
-            className="save-button"
-          >
-            SAVE
-          </button>
-        </form>
-        <div className="todo-content">
-          <ol>
-            {todos}
-          </ol>
-        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
