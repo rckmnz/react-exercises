9115f9fbe2390190f9841a1fc3a917336d430102
rckmnz <rckmnz05@gmail.com>
2018-04-07 02:27:20 -0400

    all componentes created

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Card from "./Card";
+import cardData from "./cardMetaData";
+import "./App.css";
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { shuffle(cardData) };
+  }
   render() {
+    const Cardstate = {
+      HIDING: 0,
+      SHOWING: 1,
+      MATCHING: 2
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <NavBar />
+        <Card />
       </div>
     );
   }
---
+++ memory-game/src/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <h1>Memory Game</h1>
+        <button>New Game</button>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
