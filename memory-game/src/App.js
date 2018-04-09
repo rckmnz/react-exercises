4fc96721277c9d3a30faf7790c439448f6eb34a9
rckmnz <rckmnz05@gmail.com>
2018-04-08 22:52:01 -0400

    generated all cards and click event

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -9,23 +9,46 @@ function shuffle(array) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
 }
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { cardData: shuffle(cardData) };
+    this.revealCard = this.revealCard.bind(this);
   }
+
+  revealCard(card) {
+    this.setState(prevState => {
+      prevState.cardData.map(prevCard => {
+        if (card.id === prevCard.id) {
+          prevCard.cardState = 1;
+          return prevCard;
+        } else {
+          return prevCard;
+        }
+      });
+    });
+  }
+
   render() {
-    const CardState = {
-      HIDING: 0,
-      SHOWING: 1,
-      MATCHING: 2
-    };
+    const cards = this.state.cardData.map(card => {
+      return (
+        <Card
+          key={card.id}
+          id={card.id}
+          color={card.backgroundColor}
+          state={card.cardState}
+          onClick={this.revealCard}
+        />
+      );
+    });
+
     return (
       <div className="App">
         <NavBar />
-        <Card cardData={this.state.carData}/>
+        <main>{cards}</main>
       </div>
     );
   }
--- memory-game/src/Card.js
+++ memory-game/src/Card.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e) {
+    const card = e.target;
+    console.log(card);
+    this.props.onClick(card);
+   console.log(card.style = {backgroundColor: this.props.color})
+  }
   render() {
     return (
-      <div className="card">
-      </div>  
+      <div className="card" id={this.props.id} onClick={this.handleClick} color={this.props.color} state={this.props.state}/>
     );
   }
 }
